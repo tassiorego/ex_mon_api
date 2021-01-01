8e838b642d5197749db2892c3114e7b2b6ec9742
Tassio Rego <tassiorego@gmail.com>
2021-01-01 15:16:45 -0300

    chore: create Trainer scheme

---
+++ lib/ex_mon_api/trainer.ex
@@ -0,0 +1,34 @@
+defmodule ExMonApi.Trainer do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, Ecto.UUID, autogenerade: true}
+
+  schema "trainers" do
+    field :name, :string
+    field :password_hash, :string
+    field :password, :string, virtual: true
+    timestamps()
+  end
+
+  def build(params) do
+    params
+    |> changeset()
+    |> apply_action(:insert)
+  end
+
+  @requerid_params [:name, :password]
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @requerid_params)
+    |> validate_required(@requerid_params)
+    |> validate_length(:password, min: 6)
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(%Ecto.Changeset{valid?: true, changes: %{ password: password}} = changeset) do
+    change(changeset, Argon2.add_hash(password))
+  end
+
+  defp put_pass_hash(changeset), do: changeset
+end
